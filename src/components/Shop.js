c87dbde588ad157c5f16026fb8a9fa27ec518c10
kowshik76 <kowshiksangada@gmailcom>
2025-02-22 15:26:43 +0530

    Completion of redux

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,15 +1,23 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { actionCreators } from '../state/index'
+import { bindActionCreators } from 'redux'
+import { useSelector } from 'react-redux'
 
 const Shop = () => {
+    const balance = useSelector(state => state.amount)
+
+    const dispatch = useDispatch();
+    const { depositmoney, withdrawmoney } = bindActionCreators(actionCreators, dispatch);
     return (
         <>
             <div className="container">
                 <h2>
-                    Buy These Shoes
+                    Balance of your Account
                 </h2>
-                <button className="btn btn-primary mx-2">
+                <button className="btn btn-primary mx-2" onClick={() => { (withdrawmoney(200)) }}>
                     -
-                </button>Add to cart<button className="btn btn-primary mx-2">+
+                </button><h4>{balance}</h4><button className="btn btn-primary mx-2" onClick={() => { (depositmoney(200)) }}>+
 
                 </button>
             </div>
