369efde65a5c59557e8577f3df2ddbeeac21f5a3
kowshik76 <kowshiksangada@gmailcom>
2025-02-22 11:27:41 +0530

    First Commit

---
+++ src/components/Shop.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Shop = () => {
+    return (
+        <>
+            <div className="container">
+                <h2>
+                    Buy These Shoes
+                </h2>
+                <button className="btn btn-primary mx-2">
+                    -
+                </button>Add to cart<button className="btn btn-primary mx-2">+
+
+                </button>
+            </div>
+        </>
+    )
+}
+
+export default Shop
